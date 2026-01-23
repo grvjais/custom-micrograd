c3aae1dd3f7c775619d366b3a66648f55f6a46d2
grvjais <grvjaisw@gmail.com>
2026-01-23 22:34:56 +0530

    modifying code to accept multiple activation functions

--- micrograd/neural_network.py
+++ micrograd/neural_network.py
@@ -13,20 +13,37 @@ class Module:
 
 class Neuron(Module):
 
-    def __init__(self, nin, nonlin=True):
+    def __init__(self, nin, act_fn = 'relu'):
         self.w = [AutoDiffNode(random.uniform(-1,1)) for weight in range(nin)]
         self.b = AutoDiffNode(0)
-        self.nonlin = nonlin
+        self.act_fn = act_fn
 
     def __call__(self, x):
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
-        return act.relu() if self.nonlin else act
+        
+        if self.act_fn == 'relu':
+            return act.relu()
+        
+        elif self.act_fn == 'tanh':
+            return act.tanh()
+        
+        elif self.act_fn == 'sigmoid':
+            return act.sigmoid()
+        
+        elif self.act_fn == 'linear':
+            return act
+        
+        elif self.act_fn == 'leaky_relu':
+            return act.leaky_relu()
+        
+        else:
+            return act.relu()
 
     def parameters(self):
         return self.w + [self.b]
     
     def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+        return f"{self.act_fn}Neuron({len(self.w)})"
         
 class Layer(Module):
     
@@ -45,9 +62,13 @@ class Layer(Module):
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
+    def __init__(self, nin, nouts, activation = 'relu'):
         size = [nin] + nouts
-        self.layers = [Layer(size[i], size[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        self.layers = [Layer(size[i], 
+                             size[i+1], 
+                             act_fn = 'linear' if i == len(nouts)-1 else activation
+                             ) 
+                             for i in range(len(nouts))]
 
     def __call__(self,x):
         for layer in self.layers:
